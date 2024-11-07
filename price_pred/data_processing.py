f87cd402f083f547c84a80f3449ba4f2c187bb4b
ethanleicht <ethanrleicht@gmail.com>
2024-11-07 01:49:58 -0500

    backend update

---
+++ price_pred/data_processing.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import json
+import numpy as np
+
+
+def clean_data(df):
+    
+    data = df[[
+        # 'host_since',
+        'host_location',
+        'host_response_time', 'host_response_rate', 'host_acceptance_rate',
+        'host_is_superhost',
+        'host_neighbourhood',
+        'host_listings_count',
+        # 'host_total_listings_count',
+        'host_has_profile_pic', 'host_identity_verified',
+        'neighbourhood_cleansed', 'neighbourhood_group_cleansed',
+        # 'latitude', 'longitude',
+        'property_type', 'room_type', 'accommodates', 'bathrooms',
+        'bathrooms_text', 'bedrooms', 'beds', 'amenities',
+        'minimum_nights', 'maximum_nights',
+        # 'minimum_minimum_nights', 'maximum_minimum_nights', 'minimum_maximum_nights',
+        # 'maximum_maximum_nights', 'minimum_nights_avg_ntm', 'maximum_nights_avg_ntm',
+        'has_availability',
+        # 'availability_30', 'availability_60', 'availability_90', 'availability_365',
+        'number_of_reviews',
+        # 'number_of_reviews_ltm', 'number_of_reviews_l30d',
+        # 'first_review', 'last_review',
+        'review_scores_rating', 'review_scores_accuracy',
+        'review_scores_cleanliness', 'review_scores_checkin',
+        'review_scores_communication', 'review_scores_location',
+        'review_scores_value', 'instant_bookable',
+        # 'calculated_host_listings_count',
+        # 'calculated_host_listings_count_entire_homes', 'calculated_host_listings_count_private_rooms', 'calculated_host_listings_count_shared_rooms',
+        'reviews_per_month',
+        'price']]
+
+    # Convert percentages to numbers
+    for feature in ['host_response_rate', 'host_acceptance_rate']:
+        data[feature] = data[feature].str.replace('%', '').astype(float) / 100
+    
+    # Convert t/f to integers
+    for feature in ['host_has_profile_pic', 'host_is_superhost', 'host_identity_verified', 'has_availability', 'instant_bookable']:
+        data[feature] = data[feature].map({'t': 1, 'f': 0})
+    
+    # Convert bathroom text to bathroom type
+    data['bathroom_type'] = data['bathrooms_text'].apply(lambda data: 'private' if 'private' in str(data).lower() else ('shared' if 'shared' in str(data).lower() else None))
+    data.drop(columns=['bathrooms_text'], inplace=True)
+
+    # Convert the strings to lists
+    data['amenities'] = data['amenities'].apply(json.loads)
+
+    # Flatten the list and count occurrences
+    all_amenities = [amenity for sublist in data['amenities'] for amenity in sublist]
+    amenity_counts = pd.Series(all_amenities).value_counts()
+
+    # Get the top 20 most common amenities
+    top_20_amenities = amenity_counts.nlargest(20).index.tolist()
+
+    # Create new columns for only the top 20 amenities
+    for amenity in top_20_amenities:
+        data[amenity] = data['amenities'].apply(lambda data: 1 if amenity in data else 0)
+
+    data.drop(columns=['amenities'], inplace=True)
+
+    # # Convert dates to floats
+    # for feature in ['host_since', 'first_review', 'last_review']:   
+    #     # Convert the 'date_column' to datetime
+    #     data[feature] = pd.to_datetime(data[feature])
+
+    #     # Define the reference date
+    #     reference_date = pd.to_datetime('2007-01-01')
+
+    #     # Calculate the number of days since the reference date
+    #     data[feature] = (data[feature] - reference_date).dt.days
+    
+    # Convert prices from strings to floats
+    data['price'] = data['price'].str.replace('[$,]', '', regex=True).astype(float)
+
+    # Filter rows where 'price' is not NaN
+    data = data[data['price'].notna()]
+    
+    return data
+
+
+
+nyc_listings = pd.read_csv('data/nyc_listings.csv')
+la_listings = pd.read_csv('data/la_listings.csv')
+chi_listings = pd.read_csv('data/chi_listings.csv')
+
+nyc_clean = clean_data(nyc_listings)
+la_clean = clean_data(la_listings)
+chi_clean = clean_data(chi_listings)
+
+nyc_clean.to_csv('nyc_clean.csv', index=False)
+la_clean.to_csv('la_clean.csv', index=False)
+chi_clean.to_csv('chi_clean.csv', index=False)
+
---
+++ price_pred/prediction.py
@@ -0,0 +1,124 @@
+import pandas as pd
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import pickle
+from sklearn.decomposition import PCA
+from sklearn.impute import KNNImputer
+from sklearn.decomposition import TruncatedSVD
+from scipy.sparse import csr_matrix
+import numpy as np
+
+
+def prepare_for_model(df, ft_red_method=None):
+    
+    X, y = df.drop(columns=['price']), df[['price']]
+    
+    # Identify string columns
+    string_columns = X.select_dtypes(include=['object']).columns
+
+    # Get dummy variables for all string columns
+    X_dummies = pd.get_dummies(X, columns=string_columns, drop_first=True)
+
+    # print(X_dummies.shape)
+    # print(X_dummies.dtypes)
+    
+    # Replace NaNs with a specified value (e.g., column mean or zero)
+    X_dummies.fillna(X_dummies.median(), inplace=True)  # Replace NaN with column median
+
+    # Feature reduction
+    if ft_red_method:
+        X_dummies = feature_reduction(X_dummies, y, ft_red_method)
+
+    return X_dummies, y
+
+
+
+def feature_reduction(X, y, method='corr'):
+    
+    if method=='corr':
+        # Step 1: Calculate the correlation between each feature in X and y
+        correlations = X.corrwith(y).abs()  # Compute the absolute correlation of each feature with y
+
+        # Step 2: Sort by correlation and select the top N features
+        top_n = 100  # Number of features to select
+        top_features = correlations.sort_values(ascending=False).head(top_n)
+
+        # Step 3: Select the corresponding columns from X
+        selected_columns = top_features.index
+
+        # Filter X to include only the selected columns
+        X_reduced = X[selected_columns]
+
+        # Optionally, display the top correlated features
+        print(top_features)
+
+    
+    if method=='pca':
+        pca = PCA(n_components=10)
+        X_reduced = pca.fit_transform(X)
+    
+    if method=='svd':
+        
+        # Assuming X_sparse is your sparse matrix
+        X_sparse = csr_matrix(X)
+
+        # Define the number of components you want to retain
+        n_components = 50
+
+        # Apply SVD for dimensionality reduction
+        svd = TruncatedSVD(n_components=n_components)
+        X_reduced = svd.fit_transform(X_sparse)
+
+    else:
+        pass
+
+    return X_reduced
+
+
+
+def run_xgboost_regression(X, y, test_size=0.2, random_state=42):
+    
+    # Split the data into training and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    
+    # Initialize the XGBoost regressor
+    model = xgb.XGBRegressor()
+    
+    # Train the model
+    model.fit(X_train, y_train)
+    
+    # Make predictions
+    y_pred = model.predict(X_test)
+    
+    # Calculate Mean Squared Error
+    mae = mean_absolute_error(y_test, y_pred)
+    print(f"Mean Absolute Error: {mae:.2f}")
+    
+    # Return the model and predictions
+    return model, y_pred
+
+
+
+data = pd.read_csv('nyc_clean.csv')
+X, y = prepare_for_model(data, None)
+model, y_pred = run_xgboost_regression(X, y)
+
+with open('nyc_model.pkl', 'wb') as f:
+    pickle.dump(model, f)
+
+
+data = pd.read_csv('la_clean.csv')
+X, y = prepare_for_model(data, None)
+model, y_pred = run_xgboost_regression(X, y)
+
+with open('la_model.pkl', 'wb') as f:
+    pickle.dump(model, f)
+
+
+data = pd.read_csv('chi_clean.csv')
+X, y = prepare_for_model(data, None)
+model, y_pred = run_xgboost_regression(X, y)
+
+with open('chi_model.pkl', 'wb') as f:
+    pickle.dump(model, f)
